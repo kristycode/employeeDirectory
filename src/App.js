83c97fc0baafd299654b0d54f5ed6b45f627cbe0
Kristy Farmer <kristyfarmerschool@gmail.com>
2020-06-10 23:04:31 -0500

    Troubleshooting heroku deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <Router>
       <div>
-          <Route exact path="./" component={Index} />
+          <Route exact path="/" component={Index} />
       </div>
     </Router>
 
